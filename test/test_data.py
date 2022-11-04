fc6fd9dfc3feee1978d07e170898cc9023c83fac
watronfire <snowboardman007@gmail.com>
2022-11-04 11:44:28 -0700

    Added parent coloring functionality
     Adds a new field 'parent' to the YAML specification that can be used in
     place of the 'color' field. Entries with parent field will be given a
     color that is a desaturated variant of its parent's color. This desaturation
     is progressive, so additional children lineage will have a lighter and
     lighter color. Colors are currently specified from Set1 palette but we
     can change to whatever.

--- test/test_data.py
+++ test/test_data.py
@@ -18,20 +18,24 @@ SEQS = [ENCINA_SEQS, POINTLOMA_SEQS, SOUTHBAY_SEQS]
 QPCR_COLUMNS = ["Sample_Date", "Mean viral gene copies/L"]
 
 SEQ_CONFIG = "plot_config.yml"
+ALPHA_DELTA = 0.15
 
 @pytest.fixture
 def qpcr_files():
     return [(file, pd.read_csv( file, parse_dates=["Sample_Date"] )) for file in QPCR]
 
+
 @pytest.fixture
 def seq_files():
     return [(file, pd.read_csv( file, parse_dates=["Date"] )) for file in SEQS]
+
 
 @pytest.fixture
 def seq_config():
     with open( SEQ_CONFIG, "r" ) as f:
         config = yaml.load( f, Loader=yaml.FullLoader )
     return config
+
 
 @pytest.fixture
 def seq_columns( seq_config ):
@@ -41,22 +45,51 @@ def seq_columns( seq_config ):
             columns.extend( seq_config[key]["members"] )
     return columns
 
+
 def test_qpcr_contain_correct_colums( qpcr_files ):
     for file, df in qpcr_files:
         assert all( df.columns == QPCR_COLUMNS ), f"{file} does not have the right columns names. Want: {QPCR_COLUMNS} GOT: {df.columns}"
+
 
 def test_qpcr_date_is_correct( qpcr_files ):
     for file, df in qpcr_files:
         assert ptypes.is_datetime64_any_dtype( df["Sample_Date"] ), f"{file} date column is incorrect dtype. Want: dtype('<M8[ns]') Got: {df['Sample_Date'].dtype}"
         assert ptypes.is_numeric_dtype( df["Mean viral gene copies/L"] ), f"{file} gene copies column is not numeric"
 
+
 def test_other_entry_in_config( seq_config ):
     assert "Other" in seq_config, "YAML is not complete. Does not contain 'Other' entry."
 
+
 def test_correct_keys_in_config( seq_config ):
-    for key in seq_config.keys() :
-        for value in ["name", "members", "color"] :
+    for key in seq_config.keys():
+        for value in ["name", "members"]:
             assert value in seq_config[key], f"YAML entry {key} is not complete. Does not contain '{value}' entry."
+        if "color" not in seq_config[key]:
+            assert "parent" in seq_config[key], f"YAML entry {key} is incomplete. Must specify either a 'color' or 'parent' entry."
+        else:
+            assert "parent" not in seq_config[key], f"YAML entry {key} must contain either a 'color' and 'parent' entry, but not both."
+
+
+def test_parent_in_config( seq_config ):
+    for key in seq_config.keys():
+        if 'parent' in seq_config[key]:
+            assert seq_config[key]['parent'] in seq_config.keys(), f"YAML entry {key} specifies a parent ({seq_config[key]['parent']}) that does not exist."
+
+
+def test_if_too_many_children( seq_config ):
+    child_dict = dict()
+    for key in seq_config.keys():
+        if 'parent' in seq_config[key]:
+            parent = seq_config[key]['parent']
+            if parent in child_dict:
+                child_dict[parent] += 1
+            else:
+                child_dict[parent] = 1
+    max_children = int( 1 / ALPHA_DELTA )
+    for parent, children in child_dict.items():
+        assert children <= max_children, '{parent} has more children lineages ({children}) than the alpha value allows ({max_children}). Alpha decrease will pass 0.'
+
 
 # All the files are concated at some point so we just need columns to be present in one file or another.
 def test_seqs_contain_requested_columns( seq_files, seq_columns ):
@@ -66,6 +99,7 @@ def test_seqs_contain_requested_columns( seq_files, seq_columns ):
     total_cols = set( total_cols )
     for variant in seq_columns:
         assert variant in total_cols, f"No sequence file contains column {variant} as request in config."
+
 
 def test_seqs_columns_are_correct_format( seq_files, seq_columns ):
     for file, df in seq_files:
